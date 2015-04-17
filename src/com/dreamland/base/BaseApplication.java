6af9d19d8877e3797d7623885fb48459f4a4f13a
double0291 <double0291@126.com>
2015-04-17 17:12:33 +0800

    添加图片异步加载器

--- src/com/dreamland/base/BaseApplication.java
+++ src/com/dreamland/base/BaseApplication.java
@@ -3,11 +3,19 @@ package com.dreamland.base;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.drawable.BitmapDrawable;
 import android.text.TextUtils;
+import android.view.View;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.ImageLoader.ImageContainer;
 import com.android.volley.toolbox.Volley;
+import com.dreamland.R;
 import com.dreamland.database.EntityManagerFactory;
+import com.dreamland.util.Constants.HttpCmd;
+import com.dreamland.util.cache.BitmapCache;
 
 public class BaseApplication extends Application {
     private static String PREFERENCE_NAME = "info";
@@ -22,6 +30,8 @@ public class BaseApplication extends Application {
 
     // Volley网络请求
     public RequestQueue mRequestQueue;
+    // 异步图片加载器
+ 	public ImageLoader mImageLoader;
 
     @Override
     public void onCreate() {
@@ -36,6 +46,7 @@ public class BaseApplication extends Application {
         }
 
         mRequestQueue = Volley.newRequestQueue(this);
+        mImageLoader = new ImageLoader(mRequestQueue, new BitmapCache());
     }
 
     public String getUin() {
@@ -54,4 +65,25 @@ public class BaseApplication extends Application {
         }
         return mEntityManagerFactory;
     }
+    
+    public void loadImage(View view, String url) {
+		view.setBackgroundDrawable(mApp.getResources().getDrawable(R.drawable.image_default));
+		mImageLoader.get(url, getImageListener(view));
+	}
+
+	private static ImageLoader.ImageListener getImageListener(final View view) {
+		return new ImageLoader.ImageListener() {
+			@Override
+			public void onErrorResponse(HttpCmd cmd, VolleyError error) {
+				view.setBackgroundDrawable(mApp.getResources().getDrawable(R.drawable.image_fail));
+			}
+
+			@Override
+			public void onResponse(ImageContainer response, boolean isImmediate) {
+				if (response.getBitmap() != null) {
+					view.setBackgroundDrawable(new BitmapDrawable(response.getBitmap()));
+				}
+			}
+		};
+	}
 }
